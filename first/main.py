e42c6a16c50664cdea2c5e551efd82bbcf045352
xbc <xu,bocheng@outlook.com>
2021-05-17 15:20:33 +0800

    一些新的修改

--- first/main.py
+++ first/main.py
@@ -2,7 +2,7 @@ from first.dish import Dish
 from first.user import User
 from first.plate1st import Plate1st
 from database import Database
-from ImageHandle import *
+import ImageHandle
 
 import cv2
 from multiprocessing import Process, Queue
@@ -21,23 +21,6 @@ def pay(dish_prices: float, user_balance: float, db: Database):
     :return:
     """
     pass
-
-def splitImg(image):
-    # 先将图像转化成灰度，再转化成二值图像
-    mask = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
-    _, mask = cv2.threshold(mask, 185, 255, cv2.THRESH_BINARY)
-    # 检测边缘
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    images = []
-    for c in contours:
-        _, r = cv2.minEnclosingCircle(c)  # 找到最小圆，并返回圆心坐标和半径
-        x, y, w, h = cv2.boundingRect(c)
-        # x, y, r = (int(x), int(y), int(r))
-        if 100 < r < 300:
-            img_cut = image[y: y + h, x: x + w]
-            # cv2.imwrite('img_cut.jpg', img_cut)
-            images.append(img_cut)
-    return images
 
 def screen_show():
     cap = cv2.VideoCapture(0)
