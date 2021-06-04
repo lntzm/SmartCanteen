9bda304671fa79b971648370f685cc7f303d3358
lntzm <275262559@qq.com>
2021-06-04 13:34:07 +0800

    新增加载模型进行预测

---
+++ dish/predict.py
@@ -0,0 +1,34 @@
+import time
+
+import torch
+from torchvision import transforms
+from PIL import Image
+
+
+def predict(model, transform, img_path):
+    model.eval()
+    image = Image.open(img_path)
+    image = transform(image)
+    image = image.view(1, 3, 224, 224)
+    image = image.to(torch.device("cuda:0"))
+    output = model(image)
+    _, prediction = torch.max(output, 1)
+    return prediction.tolist()[0]
+
+
+if __name__ == '__main__':
+    img_path = './dish/data/7/7_0.jpg'
+    device = torch.device("cuda:0")
+    model = torch.load('./dish/model/model.pkl')
+    # model.to(device)
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5])
+    ])
+    start = time.time()
+    prediction = predict(model, transform, img_path)
+    print(prediction)
+    print(time.time() - start)
+
